97cdbcbf9ada2164036a71e06a7115d0bf1f34cd
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-08-17 18:47:38 +0700

    Add files via upload

---
+++ lib/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFD92D20);
+  static const primaryDark = Color(0xFFB42318);
+  static const surfaceSoft = Color(0xFFF8F9FB);
+  static const text = Color(0xFF182230);
+  static const muted = Color(0xFF667085);
+  static const border = Color(0xFFE4E7EC);
+}
