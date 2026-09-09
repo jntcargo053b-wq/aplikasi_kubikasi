705b76b75fb30b2bc3c1ff2aea2afdb5f7381183
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-09-09 20:02:14 +0700

    feat: launch nextcube through first-run welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'app_theme.dart';
 import 'screens/home_screen.dart';
+import 'screens/welcome_screen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const KubikasiApp());
 }
@@ -14,9 +16,31 @@ class KubikasiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Kalkulator Kubikasi',
+      title: 'nextcube',
       theme: buildAppTheme(),
-      home: const HomeScreen(),
+      home: const _LaunchGate(),
+    );
+  }
+}
+
+class _LaunchGate extends StatelessWidget {
+  const _LaunchGate();
+
+  Future<bool> _seen() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('nextcube_welcome_seen') ?? false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _seen(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+        return snapshot.data == true ? const HomeScreen() : const WelcomeScreen();
+      },
     );
   }
 }
